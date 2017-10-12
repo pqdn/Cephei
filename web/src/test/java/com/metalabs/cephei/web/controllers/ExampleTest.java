7ea94e08572c1a0679de8116a0c86cff46bd1fd2
Babanin Maxim <pqdn@yandex.ru>
2017-10-12 20:53:04 +0300

    commits emplecontroller test

--- web/src/test/java/com/metalabs/cephei/web/controllers/ExampleTest.java
+++ web/src/test/java/com/metalabs/cephei/web/controllers/ExampleTest.java
@@ -1,24 +1,24 @@
 package com.metalabs.cephei.web.controllers;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.web.client.TestRestTemplate;
+//import org.springframework.test.context.junit.jupiter.SpringExtension;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//
+//@ExtendWith(SpringExtension.class)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ExampleTest {
 
-    @Autowired
-    private TestRestTemplate restTemplate;
-
-    @Test
-    public void home() {
-        String body = this.restTemplate.getForObject("/rest/hello", String.class);
-        assertEquals(body, "Hello world");
-    }
+//    @Autowired
+//    private TestRestTemplate restTemplate;
+//
+//    @Test
+//    public void home() {
+//        String body = this.restTemplate.getForObject("/rest/hello", String.class);
+//        assertEquals(body, "Hello world");
+//    }
 }

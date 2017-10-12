bdbfe8f2fbbadbb860987ca968c8fb11445b1624
Babanin Maxim <pqdn@yandex.ru>
2017-10-13 01:09:18 +0300

    change version spring to 5.0.0 release

--- web/src/test/java/com/metalabs/cephei/web/controllers/ExampleTest.java
+++ web/src/test/java/com/metalabs/cephei/web/controllers/ExampleTest.java
@@ -1,24 +1,26 @@
 package com.metalabs.cephei.web.controllers;
 
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.web.client.TestRestTemplate;
-//import org.springframework.test.context.junit.jupiter.SpringExtension;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//@ExtendWith(SpringExtension.class)
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class ExampleTest {
 
-//    @Autowired
-//    private TestRestTemplate restTemplate;
-//
-//    @Test
-//    public void home() {
-//        String body = this.restTemplate.getForObject("/rest/hello", String.class);
-//        assertEquals(body, "Hello world");
-//    }
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void home() {
+        String body = this.restTemplate.getForObject("/rest/hello", String.class);
+        assertThat(body).isEqualTo("Hello World");
+    }
 }

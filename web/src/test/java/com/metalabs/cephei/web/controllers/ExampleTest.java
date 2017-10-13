8c5d45850deae4dee8e85a03826bcb7a589f4f74
Babanin Maxim <pqdn@yandex.ru>
2017-10-13 14:25:54 +0300

    fix version maven-surefire-plugin from 2.20 to 2.19. 2.20 is bad version

--- web/src/test/java/com/metalabs/cephei/web/controllers/ExampleTest.java
+++ web/src/test/java/com/metalabs/cephei/web/controllers/ExampleTest.java
@@ -1,18 +1,16 @@
 package com.metalabs.cephei.web.controllers;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ExampleTest {
 
     @Autowired
@@ -21,6 +19,6 @@ public class ExampleTest {
     @Test
     public void home() {
         String body = this.restTemplate.getForObject("/rest/hello", String.class);
-        assertThat(body).isEqualTo("Hello World");
+        assertEquals(body, "Hello world");
     }
 }

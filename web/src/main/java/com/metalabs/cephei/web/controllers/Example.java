9752ff67f6399e98b396b0f2d978304cdff2f587
Evgeni Dobrynin <jd2050@yandex.ru>
2017-10-16 10:17:06 +0300

    Добавлены конфиги БД в application.properties. (#11)
    * Добавлены конфиги БД в application.properties. По адресу "...rest/pseudobot"  можно получить тестовые данные от базы
    
    * fix spring data dependency, change java version to 8
    
    * change repository base interface

--- web/src/main/java/com/metalabs/cephei/web/controllers/Example.java
+++ web/src/main/java/com/metalabs/cephei/web/controllers/Example.java
@@ -1,24 +1,35 @@
 package com.metalabs.cephei.web.controllers;
 
+import com.metalabs.cephei.web.model.PseudoBot;
+import com.metalabs.cephei.web.service.PseudoBotService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class Example {
 
+    @Autowired
+    private PseudoBotService pseudoBotService;
 
     @RequestMapping("/hello")
-    String home() {
+    String example() {
         return "Hello world";
     }
 
 
     @RequestMapping("/example")
-    List<Item> home2() {
-        List<Item> items = List.of(new Item(12, "Vasay"), new Item(13, "Petya"));
+    List<Item> example2() {
+        List<Item> items = Arrays.asList(new Item(12, "Vasay"), new Item(13, "Petya"));
         return items;
+    }
+
+    @RequestMapping("/pseudobot")
+    List<PseudoBot> example3() {
+        return pseudoBotService.findAll();
     }
 
     class Item{

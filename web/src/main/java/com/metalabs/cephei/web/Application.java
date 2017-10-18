c32e04d14f656605840a809a310a5981fba2be6a
Babanin Maxim <pqdn@yandex.ru>
2017-10-18 08:57:57 +0300

    fix import

--- web/src/main/java/com/metalabs/cephei/web/Application.java
+++ web/src/main/java/com/metalabs/cephei/web/Application.java
@@ -2,15 +2,6 @@ package com.metalabs.cephei.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcRegistrationsAdapter;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.annotation.AnnotationUtils;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.condition.PatternsRequestCondition;
-import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
-
-import java.lang.reflect.Method;
 
 @SpringBootApplication
 public class Application {

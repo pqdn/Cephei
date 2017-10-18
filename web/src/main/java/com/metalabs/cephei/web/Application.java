4477b79c8f3d40fc7c3a0e49dffbb5f593e6cfff
Babanin Maxim <pqdn@yandex.ru>
2017-10-18 08:53:28 +0300

    change configs

--- web/src/main/java/com/metalabs/cephei/web/Application.java
+++ web/src/main/java/com/metalabs/cephei/web/Application.java
@@ -17,32 +17,4 @@ public class Application {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
-
-    @Bean
-    public WebMvcRegistrationsAdapter webMvcRegistrationsHandlerMapping() {
-        return new WebMvcRegistrationsAdapter() {
-            @Override
-            public RequestMappingHandlerMapping getRequestMappingHandlerMapping() {
-                return new RequestMappingHandlerMapping() {
-                    private final static String API_BASE_PATH = "rest";
-
-                    @Override
-                    protected void registerHandlerMethod(Object handler, Method method, RequestMappingInfo mapping) {
-                        Class<?> beanType = method.getDeclaringClass();
-                        if (AnnotationUtils.findAnnotation(beanType, RestController.class) != null) {
-                            PatternsRequestCondition apiPattern = new PatternsRequestCondition(API_BASE_PATH)
-                                    .combine(mapping.getPatternsCondition());
-
-                            mapping = new RequestMappingInfo(mapping.getName(), apiPattern,
-                                    mapping.getMethodsCondition(), mapping.getParamsCondition(),
-                                    mapping.getHeadersCondition(), mapping.getConsumesCondition(),
-                                    mapping.getProducesCondition(), mapping.getCustomCondition());
-                        }
-
-                        super.registerHandlerMethod(handler, method, mapping);
-                    }
-                };
-            }
-        };
-    }
 }
